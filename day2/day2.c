e6346289833a8da0734b243c1093cc41a27e3e7a
Robert D Herb <github@robertdherb.com>
2016-05-02 21:48:26 -0500

    Another month, another half-hour of half-assed coding ;)

--- day2/day2.c
+++ day2/day2.c
@@ -5,6 +5,7 @@
 #define MAX_STR 20
 #define SUCCESS 0
 #define FAIL -1
+#define NUM_DIMENSIONS 3 // LOL
 
 
 int do_the_math( int w, int l, int h )
@@ -31,11 +32,35 @@ int do_the_math( int w, int l, int h )
 }
 
 
-int get_file( const char fileName[], char * theStrings[] )
+int break_it_down( char *theStrings[], int numLines )
+{
+
+	char thisNum[MAX_STR];
+	int row = 0, character = 0, dimension, digit = 0, lineNums[NUM_DIMENSIONS];
+
+	for( row = 0; row < numLines; ++row ) {
+		dimension = 0;
+		for( character = 0; character < MAX_STR; ++character ) {
+			if( theStrings[row][character] != 'x' ) {
+				thisNum[digit] = theStrings[row][character];
+				++digit;
+			}
+			else {
+				digit = 0;
+				lineNums[dimension] = atoi( thisNum );
+				++dimension;
+			}
+		}
+	}
+
+}
+
+
+int get_file( const char fileName[], char *theStrings[] )
 {
 
 	char ch;
-	int numLines = 0, returnCode;
+	int numLines = 0, returnCode, i = 0, j = 0;
 	FILE *theFile;
 
 	theFile = fopen( fileName, "r" );
@@ -51,14 +76,28 @@ int get_file( const char fileName[], char * theStrings[] )
 	// Allocate space
 		theStrings = malloc( numLines * sizeof( char[MAX_STR] ) );
 	// Populate theStrings
+		while( !feof( theFile ) ) {
+			theStrings[i][j] = fgetc( theFile );
+			if( theStrings[i][j] == '\n' ) {
+				theStrings[i][j] = '\0';
+				++i;
+				j = -1; // No need for an else, just always increment!
+			}
+			++j;
+		}
 
+		/* This function will call some other functions that will actually do
+		 * everything I want to happen to these strings.
+		 */
+		break_it_down( theStrings, numLines );
+
+		free( theStrings );
 		fclose( theFile );
 	}
 	else {
 		returnCode = FAIL;
 	}
 	// Return
-	printf( "%d\n", numLines );
 	return returnCode;
 }
 
